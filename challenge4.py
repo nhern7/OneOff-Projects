48d38583c513fa255a4bb651411b53277bcbe22d
Naomi Hernandez <47608592+nhern7@users.noreply.github.com>
2021-06-07 22:27:19 -0500

    Delete challenge4.py

--- challenge4.py
+++
@@ -1,9 +0,0 @@
-entry = (input("Enter a message:"))
-
-x = int(input("Enter a number:"))
-
-print (entry [:x])
-##show everything x place into the string
-
-print (entry [x:])
-##show everything after x place into the string
